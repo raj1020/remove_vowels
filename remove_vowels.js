85b663cc6087348b794d644a584448b5bf95e2c2
raj1020 <raj.1020@hotmail.com>
2020-01-27 12:53:04 +1100

    initial commit for a javascript program that removes vowels from a string

---
+++ remove_vowels.js
@@ -0,0 +1,12 @@
+function disemvowel(str) {
+   let arr = str.split('');
+    
+    
+      return arr;
+    };
+     
+
+    console.log(disemvowel("Shut up idiot"));
+
+
+// console.log("Just a test");
